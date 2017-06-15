bf8e2272eb53b33e95a27ea3e6c624b2a97ec09a
seanleavy <seanleavy@gmail.com>
2017-06-15 16:12:37 +0100

    Create launcher and mainscreen activities. Added dims.xml to version control

---
+++ app/src/main/java/com/infinitespacecanal/sean/infinitespacecanal/ui/activities/HomescreenActivity.java
@@ -0,0 +1,15 @@
+package com.infinitespacecanal.sean.infinitespacecanal.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.infinitespacecanal.sean.infinitespacecanal.R;
+
+public class HomescreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homescreen);
+    }
+}
---
+++ app/src/main/java/com/infinitespacecanal/sean/infinitespacecanal/ui/activities/LauncherActivity.java
@@ -0,0 +1,20 @@
+package com.infinitespacecanal.sean.infinitespacecanal.ui.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.infinitespacecanal.sean.infinitespacecanal.R;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
